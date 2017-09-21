45ec0920bae820229433b03fff411fad63ec95dd
apple <lwtwan321@126.com>
2017-09-21 15:00:25 +0800

    111

--- app/src/main/java/pionner/ding/com/pioneer/MainActivity.java
+++ app/src/main/java/pionner/ding/com/pioneer/MainActivity.java
@@ -10,10 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
        //this is release fix.
-        //this hot fix.
-        //this is new feature
-        //this is 1feature
-        //this is 11
-        // this is 22
+       //111
     }
 }
